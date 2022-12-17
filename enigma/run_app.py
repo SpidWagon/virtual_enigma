1eee4dfd7fdb05eef3a01822b67946d34a882125
SpidWagon <vovacickin3386@gmail.com>
2022-12-18 00:14:49 +0300

    added cli interface

--- enigma/run_app.py
+++ enigma/run_app.py
@@ -1,35 +1,31 @@
+
+import typer
 from enigma import Enigma
 from enigma.constants import CONFIG_FILE_PATH
 
 
-RESET_CMD = "/rr"
-EXIT_CMD = "/exit"
-SHOW_CMD = "/show" 
-HELP_CMD = "/help" 
 
+
+@app.command()
+def process(text: str):
+    encryptor = Enigma.from_config(CONFIG_FILE_PATH)
+    text_enc = encryptor.process_message(text)
+    print(f"\nProccesed message:\n{text_enc}\n")
+
+@app.command()
+def show_cfg():
+    encryptor = Enigma.from_config(CONFIG_FILE_PATH)
+    encryptor.show_config()
+
+@app.command()
+def help():
+    print(
+        "This is a placeholder of help command, but I swear here will be normal help command soon"
+    )
 
 def main():
-    encryptor = Enigma.from_config(CONFIG_FILE_PATH)
-
-    while True:
-        text = input("Type message to process\n")
-        if text.lower() == EXIT_CMD:
-            print("exit")
-            exit()
-        elif text.lower() == RESET_CMD:
-            encryptor.set_rotor_config()
-            print("Config reset \n")
-        elif text.lower() == SHOW_CMD:
-            encryptor.show_config()
-        elif text.lower() == HELP_CMD:
-            print(
-                "This is a placeholder of help command, but I swear here will be normal " + 
-                "help command soon"
-            )
-        else:
-            text_enc = encryptor.process_message(text)
-            print(f'\nProccesed message:\n{text_enc}\n')
-
+    app = typer.Typer()
+    app()
 
 if __name__ == "__main__":
     main()
